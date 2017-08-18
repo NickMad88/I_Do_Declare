dbc66720a9b13769e940cabed675fce78b4b174f
Nick Madani <nickmadani@Nicks-MacBook-Pro.local>
2017-08-18 16:19:20 -0700

    final commit

---
+++ I_Do_Declare.js
@@ -0,0 +1,25 @@
+var num1 = 4;
+var num2 = -23;
+var num3 = 10
+var num4 = -5;
+var string1 = "Hello";
+var string2 = "World";
+var string3 = "!";
+var worldisflat = False;
+var tomatoisfruit = True;
+var waitwut;
+
+console.log("Another word for hey:", string1);
+console.log("Around the:", string2);
+console.log("Im so excited:", string3);
+console.log("2 plus 2 is:", num1);
+console.log("A number quite a bit below 0:", num2);
+console.log("A perfect score is a 10/?:", num3);
+console.log("A really bad score is:", num4);
+console.log("Ruby is cooler than Python:", worldisflat);
+console.log("The Coding Dojo rocks!", tomatoisfruit);
+console.log("wut", waitwut);
+
+
+
+
